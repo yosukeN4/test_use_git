6c7bc820e760879c9e3cfd8f2b979a979ceddb19
yosukeN <yousken72@gmail.com>
2024-02-29 16:59:33 +0900

    added one module and test for it.

---
+++ test_total_mock.py
@@ -0,0 +1,13 @@
+import unittest
+
+from unittest.mock import MagicMock
+
+import total
+
+
+class TestTotal(unittest.TestCase):
+    def test_calculate_total(self):
+        total.read = MagicMock()
+        total.read.return_value = [1, 2, 3]
+        result = total.calculate_total('')
+        self.assertEqual(result, 6)
